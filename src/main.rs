517c3e2b1502e7848e1ff20b847dfdb351ce19e0
Kitty Elliott <kitty@glitchcat.space>
2019-11-05 19:05:42 -0700

    changed command line argument order and implemented rotate as a trait

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+use rot_n::Rotate;
+
 fn main() {
-    let (s, n) = parse_args();
-    println!("{}", rot_n::rot_str(n, s));
+    let (n, s) = parse_args();
+    println!("{}", s.rotate_by(n));
 }
 
-fn parse_args() -> (String, isize) {
-    let mut args: Vec<_> = std::env::args().collect();
+fn parse_args() -> (isize, String) {
+    let mut args: Vec<_> = std::env::args().take(3).collect();
     if args.len() < 3 {
-        println!("no command line args given.");
+        println!("Missing command line arguments! Expected at least 2, got {}", args.len() - 1);
         std::process::exit(1);
     }
-    let n = args[2].parse()
-        .expect("Could not parse second command line argument as a number");
-    let fname = args.remove(1);
-    (fname, n)
+    let n = args[1].parse()
+        .expect("Could not parse first command line argument as a number");
+    let fname = args.remove(2);
+    (n, fname)
 }
 
 mod rot_n {
@@ -20,33 +22,40 @@ mod rot_n {
     const LOWERCASE_BOUNDS: (isize, isize) = ('a' as u8 as isize, 'z' as u8 as isize);
     const UPPERCASE_BOUNDS: (isize, isize) = ('A' as u8 as isize, 'Z' as u8 as isize);
 
-    pub fn rot_str(mut n: isize, src: String) -> String {
-        n %= RANGE;
-        let mut dest = String::with_capacity(src.capacity());
-        for c in src.chars() {
-            dest.push(rot_char(n, c));
-        }
-        dest
+    pub trait Rotate {
+        fn rotate_by(self, n: isize) -> Self;
     }
 
-    fn rot_char(n: isize, c: char) -> char {
-        assert!(0 <= RANGE && n.abs() < RANGE);
-        if c.is_ascii_alphabetic() {
-            let (start, end) =
-                if c.is_ascii_uppercase() {
-                    UPPERCASE_BOUNDS
-                } else {
-                    LOWERCASE_BOUNDS
-                };
-            let mut adjusted = n + c as u8 as isize;
-            if adjusted < start {
-                adjusted += RANGE;
-            } else if end < adjusted {
-                adjusted -= RANGE;
+    impl Rotate for String {
+        fn rotate_by(self, n: isize) -> Self {
+            let mut dest = Self::with_capacity(self.len());
+            for c in self.chars() {
+                dest.push(c.rotate_by(n));
             }
-            adjusted as u8 as char
-        } else {
-            c
+            dest
+        }
+    }
+
+    impl Rotate for char {
+        fn rotate_by(self, n: isize) -> Self {
+            assert!(n.abs() < RANGE);
+            if self.is_ascii_alphabetic() {
+                let (start, end) =
+                    if self.is_ascii_uppercase() {
+                        UPPERCASE_BOUNDS
+                    } else {
+                        LOWERCASE_BOUNDS
+                    };
+                let mut adjusted = n + self as u8 as isize;
+                if adjusted < start {
+                    adjusted += RANGE;
+                } else if end < adjusted {
+                    adjusted -= RANGE;
+                }
+                adjusted as u8 as char
+            } else {
+                self
+            }
         }
     }
 }
