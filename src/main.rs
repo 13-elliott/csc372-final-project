7e72dc4abf962c4f4b304c661c2011408e3694d2
Kitty Elliott <kitty@glitchcat.space>
2019-11-13 19:17:00 -0700

    removed "rot_n" module and Rotate trait

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rot_n::Rotate;
+const RANGE: isize = 26;
+const LOWERCASE_BOUNDS: (isize, isize) = ('a' as isize, 'z' as isize);
+const UPPERCASE_BOUNDS: (isize, isize) = ('A' as isize, 'Z' as isize);
 
 fn main() {
-    let (n, s) = parse_args();
-    println!("{}", s.rotate_by(n));
+    let (n, fname) = parse_args();
+    // TODO: file handling
 }
 
 fn parse_args() -> (isize, String) {
@@ -17,55 +19,41 @@ fn parse_args() -> (isize, String) {
     (n, fname)
 }
 
-mod rot_n {
-    const RANGE: isize = 26;
-    const LOWERCASE_BOUNDS: (isize, isize) = ('a' as isize, 'z' as isize);
-    const UPPERCASE_BOUNDS: (isize, isize) = ('A' as isize, 'Z' as isize);
-
-    pub trait Rotate {
-        fn rotate_by(self, n: isize) -> Self;
+/// Consumes `self` and produces a new String in which each char of self
+/// is rotated by `n`.
+fn rot_str(s: &mut String, mut by_n: isize) {
+    let mut dest = String::with_capacity(s.len());
+    by_n %= RANGE;
+    for c in s.chars() {
+        dest.push(rot_char(c, by_n));
     }
+    *s = dest;
+}
 
-    impl Rotate for String {
-        /// Consumes `self` and produces a new String in which each char of self
-        /// is rotated by `n`.
-        fn rotate_by(self, mut n: isize) -> Self {
-            let mut dest = Self::with_capacity(self.len());
-            n %= RANGE;
-            for c in self.chars() {
-                dest.push(c.rotate_by(n));
-            }
-            dest
-        }
-    }
-
-    impl Rotate for char {
-        /// If this char is an alphabetic ascii char,
-        /// produces that character rotated by n through the alphabet, looping
-        /// back to a when z is passed.
-        fn rotate_by(self, n: isize) -> Self {
-            assert!(n.abs() < RANGE);
-            if self.is_ascii_alphabetic() {
-                // select the appropriate upper and lower bounds
-                let (start, end) =
-                    if self.is_ascii_uppercase() {
-                        UPPERCASE_BOUNDS
-                    } else {
-                        LOWERCASE_BOUNDS
-                    };
-                // add n and adjust as necessary back into
-                // the proper range of start..=end
-                let mut adjusted = n + self as isize;
-                if adjusted < start {
-                    adjusted += RANGE;
-                } else if end < adjusted {
-                    adjusted -= RANGE;
-                }
-                adjusted as u8 as char
+/// If this char is an alphabetic ascii char,
+/// produces that character rotated by n through the alphabet, looping
+/// back to a when z is passed.
+fn rot_char(c: char, by_n: isize) -> char {
+    assert!(by_n.abs() < RANGE);
+    if c.is_ascii_alphabetic() {
+        // select the appropriate upper and lower bounds
+        let (start, end) =
+            if c.is_ascii_uppercase() {
+                UPPERCASE_BOUNDS
             } else {
-                // return self unchanged if it cannot be rotated.
-                self
-            }
+                LOWERCASE_BOUNDS
+            };
+        // add n and adjust as necessary back into
+        // the proper range of start..=end
+        let mut adjusted = by_n + c as isize;
+        if adjusted < start {
+            adjusted += RANGE;
+        } else if end < adjusted {
+            adjusted -= RANGE;
         }
+        adjusted as u8 as char
+    } else {
+        // return unchanged if it cannot be rotated.
+        c
     }
 }
